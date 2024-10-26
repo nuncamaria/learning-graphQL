95ae53fcae9de2882f27f20969655e62859bdd7f
nuncamaria <nunezcanovas@gmail.com>
2024-10-26 13:13:05 +0200

    Added buildSrc package

---
+++ buildSrc/src/main/java/ProjectConfig.kt
@@ -0,0 +1,13 @@
+object ProjectConfig {
+
+    const val APP_ID = "com.nuncamaria.learninggraphql"
+
+    const val MIN_SDK = 27
+    const val COMPILE_SDK = 34
+    const val TARGET_SDK = 34
+
+    const val VERSION_CODE = 1
+    const val VERSION_NAME = "1.0"
+
+    const val TEST_INSTRUMENTATION_RUNNER = "androidx.test.runner.AndroidJUnitRunner"
+}
